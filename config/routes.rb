592e928ad07283a0ee4c960cc6e649458b86ef12
Frederick Lazo <fredlazo@yahoo.com>
2016-07-06 07:28:36 -0700

    Created Users controller and User#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'users#show'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
