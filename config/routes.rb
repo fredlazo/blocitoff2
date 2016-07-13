46c98c506f7ade0a7a4a45f063f20c125369bb07
Frederick Lazo <fredlazo@yahoo.com>
2016-07-13 08:06:26 -0700

    Seed with Faker no confirmation rqd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root 'users#show'
+
+  get 'welcome/index' => 'welcome#index'
 
   devise_for :users
+
+  resources :users, only: [:show] do
+    resources :items, only: [:create]
+  end
+
+  root 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
